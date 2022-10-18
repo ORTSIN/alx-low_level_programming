5ea8b6cd8474a57f2557f13a057785d632151c91
ORTSIN <ortsinnanaisaac@gmail.com>
2022-10-18 16:17:32 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,18 @@
-#include "main.h"
+#ifndef main_h
+#define main_h
 
-/**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
- */
-int _islower(int c)
-{
-	return (c >= 'a' && c <= 'z');
-}
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
