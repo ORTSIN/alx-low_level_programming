f58e47c01474bc15ea6ef0c435eb07d6dd7c7723
ORTSIN <ortsinnanaisaac@gmail.com>
2022-10-19 17:28:17 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+
+#define main_h	
+
+
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-void print_time_table(int n);
 
-#endif
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
+
+
+
+#endif /* main_h */
